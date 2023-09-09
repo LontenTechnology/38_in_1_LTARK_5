d20ea0516c88c9def043699139f46a4ddc4cb09b
Lonten Technology <142191915+LontenTechnology@users.noreply.github.com>
2023-09-09 17:20:31 +0800

    Add files via upload

---
+++ Arduino Code/Lesson 10 Photo-Interrupter Module/Photo-interrupter_MODULE/Photo-interrupter_MODULE.ino
@@ -0,0 +1,21 @@
+
+
+int Led=13;//define the led's port
+int buttonpin=4; //define the port of light blocking module
+int val;//define digital variable val
+void  setup()
+{
+pinMode(Led,OUTPUT);//define digital variable val
+pinMode(buttonpin,INPUT);//define light blocking module as a output port
+}
+void  loop()
+{ val=digitalRead(buttonpin);//read the value of the digital interface 4 assigned to val 
+if(val==HIGH)//when the light blocking sensor have signal, LED blink
+{
+digitalWrite(Led,LOW);
+}
+else
+{
+digitalWrite(Led,HIGH);
+}
+}
